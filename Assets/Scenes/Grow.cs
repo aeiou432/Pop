d603095d6d10d0f318bf20b3c589f0f4ad72b349
John Guo <aeiou1228@gmail.com>
2019-05-17 00:35:53 +0800

    draw line

--- Assets/Scenes/Grow.cs
+++ Assets/Scenes/Grow.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GrowDefine {
     public static int beginHeightRate = 1;
@@ -13,6 +14,7 @@ public class GrowDefine {
 public static class GlobalValue {
     public static int MaxHeight;
     public static Block StartBlock;
+    public static Color32[] pixels = new Color32[500 * 1000];
 }
 public class Block {
     int height = 0;
@@ -97,5 +99,44 @@ public class Block {
             }
         }
         this.root = null;
+    }
+    public void Draw() {
+       
+    }
+
+    public void line(int x0, int x1, int y0, int y1, Color32[] pixels) {
+        bool steep = Mathf.Abs(y1 - y0) > Mathf.Abs(x1 - x0);
+        int tmp;
+        if (steep) {
+            tmp = x0;
+            x0 = y0;
+            y0 = tmp;
+            tmp = x1;
+            x1 = y1;
+            y1 = tmp;
+        }
+        if (x0 > x1) {
+            tmp = x0;
+            x0 = x1;
+            x1 = tmp;
+            tmp = y0;
+            y0 = y1;
+            y1 = tmp;
+        }
+        int deltax = x1 - x0;
+        int deltay = Mathf.Abs(y1 - y0);
+        float error = 0;
+        float delttaerr = deltay / deltax;
+        int ystep;
+        int y = y0;
+        if (y0 < y1) ystep = 1; else ystep = -1;
+        for (int x = x0; x <= x1; x++) {
+            if (steep) pixels[y + x * 500] = Color.white; else pixels[x + y * 500] = Color.white;
+            error = error + delttaerr;
+            if (error >= 0.5) {
+                y = y + ystep;
+                error = error - 1.0f;
+            }
+        }
     }
 }
