bf0800f5c640b2300928266890d58371d2d5d3b2
John Guo <aeiou1228@gmail.com>
2019-05-28 21:37:32 +0800

    publish

---
+++ Assets/Script/Game.cs
@@ -0,0 +1,216 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Monetization;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour {
+    public SpriteRenderer Tree;
+    public Text Score;
+    public Text Sub;
+    public Bubble ResBubble;
+    public AudioSource Audio;
+    public List<AudioSource> Audio1;
+    public List<AudioSource> Audio2;
+    public List<AudioSource> Audio3;
+    public List<Bubble> Bubbles;
+    public List<Bubble> BubblePool;
+    public float BubbleInverval = 2;
+    public float LevelInterval = 10;
+    public Texture2D Tex;
+    public Button Play;
+    public Button BackGround;
+    private int adthresholdNo = 300;
+    private int numberOnce = 1;
+    private int nowLevelTimes = 0;
+    private int number = 0;
+    private int topNumber = 0;
+    private Block treeStart;
+    private bool showAd;
+    private int audioIndex1;
+    private int audioIndex2;
+    private int audioIndex3;
+    private AudioSource nowAudio;
+    private static string touchNumber = "number";
+    private static string topHeight = "height";
+    private static string subNumber = "subNumber";
+    private static string adShow = "adShow";
+    private static string AndroidGameId = "3159824";
+    private static string iOSGameId = "3159825";
+    private static string AdName = "video";
+    private string dataPath;
+    public void Start() {
+#if UNITY_ANDROID
+        Monetization.Initialize(AndroidGameId, false);
+#else
+        Monetization.Initialize(iOSGameId, false);
+#endif
+        this.nowAudio = this.Audio;
+        this.dataPath = Application.persistentDataPath + "/save.txt";
+        this.LoadData();
+
+        if (this.treeStart == null) {
+            this.treeStart = new Block(new Vector2(GrowDefine.LOCAL_DISPLAY_WIDTH / 2, 0), Vector2.up);
+            this.treeStart.isTop = true;
+            GlobalValue.StartBlock = this.treeStart;
+        }
+        this.Tex = new Texture2D(GrowDefine.LOCAL_DISPLAY_WIDTH, GrowDefine.LOCAL_DISPLAY_HEIGHT);
+        this.Tex.filterMode = FilterMode.Trilinear;
+        this.Tree.sprite = Sprite.Create(this.Tex, new Rect(0, 0, GrowDefine.LOCAL_DISPLAY_WIDTH, GrowDefine.LOCAL_DISPLAY_HEIGHT), new Vector2(0.5f, 0.5f));
+        this.treeStart.Draw();
+        this.Tree.sprite.texture.SetPixels(GlobalValue.pixels);
+        this.Tree.sprite.texture.Apply();
+        this.OnBackgroundClick();
+    }
+    public void OnApplicationQuit() {
+        this.SaveDate();
+    }
+    public void OnApplicationFocus(bool hasFocus) {
+        if (!hasFocus) {
+            this.SaveDate();
+        }
+    }
+    // Update is called once per frame
+    void Update() {
+        /*this.treeStart.Grow();
+        this.treeStart.Draw();
+        this.Tree.sprite.texture.SetPixels(GlobalValue.pixels);
+        this.Tree.sprite.texture.Apply();*/
+    }
+
+    public void OnBackgroundClick() {
+        bool active = !this.Play.gameObject.activeSelf;
+        this.Play.gameObject.SetActive(active);
+        if (this.topNumber > 0) {
+            this.Score.text = "Top: " + this.topNumber.ToString();
+            this.Score.gameObject.SetActive(active);
+        }
+        if (GlobalValue.TreePixel > 0) {
+            this.Sub.text = "Tree: " + GlobalValue.TreePixel.ToString();
+            this.Sub.gameObject.SetActive(active);
+        }
+    }
+    public void OnPlayClick() {
+        if (this.showAd) {
+            ShowAdPlacementContent adContent = Monetization.GetPlacementContent(AdName) as ShowAdPlacementContent;
+            adContent.Show(PlayGame);
+            this.showAd = false;
+        }
+        else {
+            this.PlayGame(ShowResult.Finished);
+        }
+    }
+    private void PlayGame(ShowResult finishState) {
+        this.Play.gameObject.SetActive(false);
+        this.BackGround.gameObject.SetActive(false);
+        this.Score.gameObject.SetActive(true);
+        this.Sub.gameObject.SetActive(true);
+        this.Sub.text = GlobalValue.TreePixel.ToString();
+        this.Score.text = this.number.ToString();
+        this.RandomCreate();
+    }
+    private void RandomCreate() {
+        Bubble bubble;
+        for (int i = 0; i < this.numberOnce; i++) {
+            if (this.BubblePool.Count <= 0) {
+                bubble = GameObject.Instantiate<Bubble>(this.ResBubble, this.transform);
+                bubble.OnTouch.AddListener(this.Recycle);
+                bubble.Boo.AddListener(this.Failed);
+            }
+            else {
+                bubble = this.BubblePool[0];
+                this.BubblePool.RemoveAt(0);
+            }
+            bubble.transform.SetAsFirstSibling();
+            float randomX = Random.Range(50f, Screen.width - 50f);
+            float randomY = Random.Range(50, Screen.height - 50f);
+            Vector2 touchPosition = new Vector2(randomX, randomY);
+            bubble.Reset(touchPosition);
+            this.Bubbles.Add(bubble);
+        }
+    }
+    private void Recycle(Bubble bubble) {
+        this.number++;
+        this.Score.text = this.number.ToString();
+        this.PlaySound();
+        this.treeStart.Grow();
+        this.treeStart.Draw();
+        this.Tree.sprite.texture.SetPixels(GlobalValue.pixels);
+        this.Tree.sprite.texture.Apply();
+        this.Sub.text = GlobalValue.TreePixel.ToString();
+        this.BubblePool.Add(bubble);
+        this.Bubbles.Remove(bubble);
+        if (this.Bubbles.Count <= 0) {
+            this.nowLevelTimes++;
+            if (this.nowLevelTimes >= 10) {
+                this.numberOnce++;
+                this.nowLevelTimes = 0;
+            }
+            this.RandomCreate();
+        }
+    }
+    private void PlaySound() {
+        if (this.nowAudio.isPlaying) return;
+        int chance = Random.Range(0, 100);
+        if (this.number > 300 && chance < 10) {
+            this.nowAudio = this.Audio3[this.audioIndex3];
+            this.audioIndex3 = (this.audioIndex3 == 0) ? 1 : 0;
+        }
+        else if (this.number > 150 && chance < 25) {
+            this.nowAudio = this.Audio2[this.audioIndex2];
+            this.audioIndex2 = (this.audioIndex2 == 0) ? 1 : 0;
+        }
+        else if (this.number > 50 && chance < 50) {
+            this.nowAudio = this.Audio1[this.audioIndex1];
+            this.audioIndex1 = (this.audioIndex1 == 0) ? 1 : 0;
+        }
+        else {
+            this.nowAudio = this.Audio;
+        }
+        this.nowAudio.Play();
+    }
+    private void Failed(Bubble bubble) {
+        for(int i = this.Bubbles.Count - 1; i >= 0 ; i--) {
+            this.Bubbles[i].End();
+            this.BubblePool.Add(this.Bubbles[i]);
+            this.Bubbles.RemoveAt(i);
+        }
+        if(this.number > this.topNumber) {
+            this.topNumber = this.number;
+        }
+        if (this.number > this.adthresholdNo) {
+            this.showAd = true;
+        }
+        this.number = 0;
+        this.numberOnce = 1;
+        this.nowLevelTimes = 0;
+        this.BackGround.gameObject.SetActive(true);
+    }
+    private void SaveDate() {
+        string save = JsonConvert.SerializeObject(this.treeStart);
+        PlayerPrefs.SetInt(touchNumber, this.topNumber);
+        PlayerPrefs.SetInt(topHeight, GlobalValue.MaxHeight);
+        PlayerPrefs.SetInt(subNumber, GlobalValue.TreePixel);
+        PlayerPrefs.SetInt(adShow, this.showAd ? 1 : 0);
+        using (StreamWriter sw = new StreamWriter(dataPath)) {
+            sw.WriteLine(save);
+        }
+    }
+    private void LoadData() {
+        if (File.Exists(dataPath)) {
+            using (StreamReader sr = new StreamReader(dataPath)) {
+                string data = sr.ReadLine();
+                this.treeStart = JsonConvert.DeserializeObject<Block>(data);
+            }
+            if (this.treeStart != null) {
+                this.treeStart.Load();
+                GlobalValue.StartBlock = this.treeStart;
+            }
+            this.topNumber = PlayerPrefs.GetInt(touchNumber, this.topNumber);
+            GlobalValue.MaxHeight = PlayerPrefs.GetInt(topHeight, GlobalValue.MaxHeight);
+            GlobalValue.TreePixel = PlayerPrefs.GetInt(subNumber, GlobalValue.TreePixel);
+            this.showAd = PlayerPrefs.GetInt(adShow, 0) == 1 ? true : false;
+        }
+    }
+}
---
+++ Assets/Script/Grow.cs
@@ -0,0 +1,475 @@
+﻿using UnityEngine;
+
+public class GrowDefine {
+    public static int beginHeightRate = 1;
+    public static int endHeightRate = 199;
+    public static int lightLimit = 1200;
+    public static int minHeight = 50;
+    public static int subChance = 10;
+    public static int[] angles = new int[] { -60, 0, 60 };
+    public static int angleRange = 20;
+    public static int LOCAL_DISPLAY_WIDTH = 600;
+    public static int LOCAL_DISPLAY_HEIGHT = 1000;
+    public static Color TreeColor = new Color(1f, 1f, 1f, 1);
+    public static Color ColorGrad = new Color(0.03f, 0.03f, 0.03f, 0);
+}
+
+public static class GlobalValue {
+    public static int MaxHeight;
+    public static int TreePixel;
+    public static Block StartBlock;
+    public static Color[] pixels = new Color[GrowDefine.LOCAL_DISPLAY_WIDTH * GrowDefine.LOCAL_DISPLAY_HEIGHT];
+}
+
+public class Block {
+    public int height = 0;
+    public int width = 3;
+    public int light;
+    public bool end = false;
+    public float growChanceRate;
+    public int level = 0;
+    public int depth = 0;
+    public Vector2 angleVector;
+    public bool isTop = false;
+    public Vector2 start;
+    public Block[] subs = new Block[3];
+    Block root;
+
+    public Block(Vector2 start, Vector2 angleVector, float growChanceRate = 1f) {
+        this.start = start;
+        this.growChanceRate = growChanceRate;
+        this.angleVector = angleVector;
+    }
+    public bool Grow() {
+        if (this.light > GrowDefine.lightLimit && (this.subs[0] != null || this.subs[2] != null)) {
+            if (this.subs[0] != null) {
+                this.subs[0].Die();
+                this.subs[0] = null;
+            }
+            if (this.subs[2] != null) {
+                this.subs[2].Die();
+                this.subs[2] = null;
+            }
+            for (int i = 0; i < GlobalValue.pixels.Length; i++) {
+                GlobalValue.pixels[i] = Color.clear;
+            }
+        }
+        if (this.end) return false;
+        Vector2 endPoint = this.start + this.angleVector * this.height;
+        if (endPoint.x < 0 || endPoint.x >= GrowDefine.LOCAL_DISPLAY_WIDTH ||
+            endPoint.y < 0 || endPoint.y >= GrowDefine.LOCAL_DISPLAY_HEIGHT) {
+            this.end = true;
+            return false;
+        }
+        bool grow = false;
+        for (int i = 0; i < this.subs.Length; i++) {
+            if (this.subs[i] != null) {
+                if(this.subs[i].Grow()) {
+                    grow = true;
+                }
+            }
+        }
+        if (this.subs[1] != null) return grow;
+        float growChance = (GlobalValue.MaxHeight * 0.1f + 1) * this.growChanceRate;
+        //float growChance = this.growChanceRate;
+        if (Random.Range(0, growChance) >= 1) return grow;
+        this.height++;
+        GlobalValue.TreePixel++;
+        if (this.isTop) {
+            GlobalValue.MaxHeight++;
+            GlobalValue.StartBlock.SetLight();
+        }
+        if (this.height < GrowDefine.minHeight) return true;
+        if (Random.Range(0, 100) >= GrowDefine.subChance) return true;
+
+        for (int i = 0; i < this.subs.Length; i++) {
+            int leftAngle = GrowDefine.angles[i] - GrowDefine.angleRange;
+            int rightAngle = GrowDefine.angles[i] + GrowDefine.angleRange + 1;
+            if (this.isTop && this.angleVector.x > 0) {
+                leftAngle = GrowDefine.angles[i] - GrowDefine.angleRange / 4;
+            }
+            else if (this.isTop && this.angleVector.x < 0) {
+                rightAngle = GrowDefine.angles[i] + GrowDefine.angleRange / 4 + 1;
+            }
+
+            int angle = Random.Range(leftAngle, rightAngle);
+            Quaternion rotate = Quaternion.AngleAxis(angle, Vector3.forward);
+            Vector2 angleVector = rotate * this.angleVector;
+            if (angleVector.y < -0.5f) continue;
+            Vector2 end = this.start + this.angleVector * this.height;
+            float chance = this.growChanceRate;
+            if (i != 1) {
+                chance = growChanceRate * 2;
+            }
+            Block newBlock = new Block(end, angleVector, chance);
+            newBlock.level = this.level + 1;
+            newBlock.root = this;
+            this.subs[i] = newBlock;
+        }
+        this.height++;
+        if (this.isTop) {
+            this.isTop = false;
+            this.subs[1].isTop = true;
+        }
+        this.depth++;
+        this.width = depth * 2 + 3;
+        if (this.root != null) {
+            this.root.ReCalDepth();
+        }
+        return true;
+    }
+    public void ReCalDepth() {
+        for (int i = 0; i < this.subs.Length; i++) {
+            if (this.subs[i] != null) {
+                if (this.subs[i].depth >= this.depth) {
+                    this.depth = this.subs[i].depth + 1;
+                }
+            }
+        }
+        this.width = depth * 2 + 3;
+        if (this.root != null) {
+            this.root.ReCalDepth();
+        }
+    }
+    public void SetLight() {
+        this.light = GlobalValue.MaxHeight - (int)this.start.y;
+        for (int i = 0; i < this.subs.Length; i++) {
+            if (this.subs[i] != null) {
+                this.subs[i].SetLight();
+            }
+        }
+    }
+    public void Die() {
+        for (int i = 0; i < this.subs.Length; i++) {
+            if (this.subs[i] != null) {
+                this.subs[i].Die();
+                this.subs[i] = null;
+            }
+        }
+        this.root = null;
+    }
+    public enum Overlap {
+        LINE_OVERLAP_NONE = 0x000,
+        LINE_OVERLAP_MAJOR = 0x001,
+        LINE_OVERLAP_MINOR = 0x002,
+    }
+    public enum ThicknessMod {
+        LINE_THICKNESS_MIDDLE,
+        LINE_THICKNESS_DRAW_CLOCKWISE,
+        LINE_THICKNESS_DRAW_COUNTERCLOCKWISE
+    }
+    public void Draw() {
+        for (int i = 0; i < this.subs.Length; i++) {
+            if (this.subs[i] != null) {
+                this.subs[i].Draw();
+            }
+        }
+        if (this.height <= 0) return;
+        Vector2 end = this.start + this.angleVector * this.height;
+        Color color = GrowDefine.TreeColor - GrowDefine.ColorGrad * this.level;
+        this.DrawThickLine(Mathf.RoundToInt(this.start.x), Mathf.RoundToInt(this.start.y), Mathf.RoundToInt(end.x), Mathf.RoundToInt(end.y), this.width, ThicknessMod.LINE_THICKNESS_MIDDLE, color);
+    }
+    public void DrawPixel(int x, int y, Color color) {
+        GlobalValue.pixels[x + y * GrowDefine.LOCAL_DISPLAY_WIDTH] = color;
+    }
+    public void FillRect(int aXStart, int aYStart, int aXEnd, int aYEnd, Color aColor) {
+        for (int x = aXStart; x <= aXEnd; x++) {
+            for (int y = aYStart; y <= aYEnd; y++) {
+                this.DrawPixel(x, y, aColor);
+            }
+        }
+    }
+    public void DrawLineOverlap(int aXStart, int aYStart, int aXEnd, int aYEnd, Overlap aOverlap,
+        Color aColor) {
+        int tDeltaX, tDeltaY, tDeltaXTimes2, tDeltaYTimes2, tError, tStepX, tStepY;
+
+        /*
+         * Clip to display size
+         */
+        if (aXStart >= GrowDefine.LOCAL_DISPLAY_WIDTH) {
+            return;
+        }
+        if (aXStart < 0) {
+            return;
+        }
+        if (aXEnd >= GrowDefine.LOCAL_DISPLAY_WIDTH) {
+            aXEnd = GrowDefine.LOCAL_DISPLAY_WIDTH - 1;
+        }
+        if (aXEnd < 0) {
+            aXEnd = 0;
+        }
+        if (aYStart >= GrowDefine.LOCAL_DISPLAY_HEIGHT) {
+            return;
+        }
+        if (aYStart < 0) {
+            return;
+        }
+        if (aYEnd >= GrowDefine.LOCAL_DISPLAY_HEIGHT) {
+            aYEnd = GrowDefine.LOCAL_DISPLAY_HEIGHT - 1;
+        }
+        if (aYEnd < 0) {
+            aYEnd = 0;
+        }
+
+        if ((aXStart == aXEnd) || (aYStart == aYEnd)) {
+            //horizontal or vertical line -> fillRect() is faster
+            this.FillRect(aXStart, aYStart, aXEnd, aYEnd, aColor);
+        }
+        else {
+            //calculate direction
+            tDeltaX = aXEnd - aXStart;
+            tDeltaY = aYEnd - aYStart;
+            if (tDeltaX < 0) {
+                tDeltaX = -tDeltaX;
+                tStepX = -1;
+            }
+            else {
+                tStepX = +1;
+            }
+            if (tDeltaY < 0) {
+                tDeltaY = -tDeltaY;
+                tStepY = -1;
+            }
+            else {
+                tStepY = +1;
+            }
+            tDeltaXTimes2 = tDeltaX << 1;
+            tDeltaYTimes2 = tDeltaY << 1;
+            //draw start pixel
+            this.DrawPixel(aXStart, aYStart, aColor);
+            if (tDeltaX > tDeltaY) {
+                // start value represents a half step in Y direction
+                tError = tDeltaYTimes2 - tDeltaX;
+                while (aXStart != aXEnd) {
+                    // step in main direction
+                    aXStart += tStepX;
+                    if (tError >= 0) {
+                        if ((aOverlap & Overlap.LINE_OVERLAP_MAJOR) == Overlap.LINE_OVERLAP_MAJOR) {
+                            // draw pixel in main direction before changing
+                            this.DrawPixel(aXStart, aYStart, aColor);
+                        }
+                        // change Y
+                        aYStart += tStepY;
+                        if ((aOverlap & Overlap.LINE_OVERLAP_MINOR) == Overlap.LINE_OVERLAP_MINOR) {
+                            // draw pixel in minor direction before changing
+                            this.DrawPixel(aXStart - tStepX, aYStart, aColor);
+                        }
+                        tError -= tDeltaXTimes2;
+                    }
+                    tError += tDeltaYTimes2;
+                    this.DrawPixel(aXStart, aYStart, aColor);
+                }
+            }
+            else {
+                tError = tDeltaXTimes2 - tDeltaY;
+                while (aYStart != aYEnd) {
+                    aYStart += tStepY;
+                    if (tError >= 0) {
+                        if ((aOverlap & Overlap.LINE_OVERLAP_MAJOR) == Overlap.LINE_OVERLAP_MAJOR) {
+                            // draw pixel in main direction before changing
+                            this.DrawPixel(aXStart, aYStart, aColor);
+                        }
+                        aXStart += tStepX;
+                        if ((aOverlap & Overlap.LINE_OVERLAP_MINOR) == Overlap.LINE_OVERLAP_MINOR) {
+                            // draw pixel in minor direction before changing
+                            this.DrawPixel(aXStart, aYStart - tStepY, aColor);
+                        }
+                        tError -= tDeltaYTimes2;
+                    }
+                    tError += tDeltaXTimes2;
+                    this.DrawPixel(aXStart, aYStart, aColor);
+                }
+            }
+        }
+    }
+
+    public void DrawThickLine(int aXStart, int aYStart, int aXEnd, int aYEnd, int aThickness,
+        ThicknessMod aThicknessMode, Color aColor) {
+        int i, tDeltaX, tDeltaY, tDeltaXTimes2, tDeltaYTimes2, tError, tStepX, tStepY;
+
+        if (aThickness <= 1) {
+            this.DrawLineOverlap(aXStart, aYStart, aXEnd, aYEnd, Overlap.LINE_OVERLAP_NONE, aColor);
+        }
+        /*
+         * Clip to display size
+         */
+        if (aXStart >= GrowDefine.LOCAL_DISPLAY_WIDTH) {
+            aXStart = GrowDefine.LOCAL_DISPLAY_WIDTH - 1;
+        }
+        if (aXStart < 0) {
+            aXStart = 0;
+        }
+        if (aXEnd >= GrowDefine.LOCAL_DISPLAY_WIDTH) {
+            aXEnd = GrowDefine.LOCAL_DISPLAY_WIDTH - 1;
+        }
+        if (aXEnd < 0) {
+            aXEnd = 0;
+        }
+        if (aYStart >= GrowDefine.LOCAL_DISPLAY_HEIGHT) {
+            aYStart = GrowDefine.LOCAL_DISPLAY_HEIGHT - 1;
+        }
+        if (aYStart < 0) {
+            aYStart = 0;
+        }
+        if (aYEnd >= GrowDefine.LOCAL_DISPLAY_HEIGHT) {
+            aYEnd = GrowDefine.LOCAL_DISPLAY_HEIGHT - 1;
+        }
+        if (aYEnd < 0) {
+            aYEnd = 0;
+        }
+
+        /**
+         * For coordinate system with 0.0 top left
+         * Swap X and Y delta and calculate clockwise (new delta X inverted)
+         * or counterclockwise (new delta Y inverted) rectangular direction.
+         * The right rectangular direction for LINE_OVERLAP_MAJOR toggles with each octant
+         */
+        tDeltaY = aXEnd - aXStart;
+        tDeltaX = aYEnd - aYStart;
+        // mirror 4 quadrants to one and adjust deltas and stepping direction
+        bool tSwap = true; // count effective mirroring
+        if (tDeltaX < 0) {
+            tDeltaX = -tDeltaX;
+            tStepX = -1;
+            tSwap = !tSwap;
+        }
+        else {
+            tStepX = +1;
+        }
+        if (tDeltaY < 0) {
+            tDeltaY = -tDeltaY;
+            tStepY = -1;
+            tSwap = !tSwap;
+        }
+        else {
+            tStepY = +1;
+        }
+        tDeltaXTimes2 = tDeltaX << 1;
+        tDeltaYTimes2 = tDeltaY << 1;
+        Overlap tOverlap;
+        // adjust for right direction of thickness from line origin
+        int tDrawStartAdjustCount = aThickness / 2;
+        if (aThicknessMode == ThicknessMod.LINE_THICKNESS_DRAW_COUNTERCLOCKWISE) {
+            tDrawStartAdjustCount = aThickness - 1;
+        }
+        else if (aThicknessMode == ThicknessMod.LINE_THICKNESS_DRAW_CLOCKWISE) {
+            tDrawStartAdjustCount = 0;
+        }
+
+        // which octant are we now
+        if (tDeltaX >= tDeltaY) {
+            if (tSwap) {
+                tDrawStartAdjustCount = (aThickness - 1) - tDrawStartAdjustCount;
+                tStepY = -tStepY;
+            }
+            else {
+                tStepX = -tStepX;
+            }
+            /*
+             * Vector for draw direction of lines is rectangular and counterclockwise to original line
+             * Therefore no pixel will be missed if LINE_OVERLAP_MAJOR is used
+             * on changing in minor rectangular direction
+             */
+            // adjust draw start point
+            tError = tDeltaYTimes2 - tDeltaX;
+            for (i = tDrawStartAdjustCount; i > 0; i--) {
+                // change X (main direction here)
+                aXStart -= tStepX;
+                aXEnd -= tStepX;
+                if (tError >= 0) {
+                    // change Y
+                    aYStart -= tStepY;
+                    aYEnd -= tStepY;
+                    tError -= tDeltaXTimes2;
+                }
+                tError += tDeltaYTimes2;
+            }
+            //draw start line
+            this.DrawLineOverlap(aXStart, aYStart, aXEnd, aYEnd, Overlap.LINE_OVERLAP_NONE, aColor);
+            // draw aThickness lines
+            tError = tDeltaYTimes2 - tDeltaX;
+            for (i = aThickness; i > 1; i--) {
+                // change X (main direction here)
+                aXStart += tStepX;
+                aXEnd += tStepX;
+                tOverlap = Overlap.LINE_OVERLAP_NONE;
+                if (tError >= 0) {
+                    // change Y
+                    aYStart += tStepY;
+                    aYEnd += tStepY;
+                    tError -= tDeltaXTimes2;
+                    /*
+                     * change in minor direction reverse to line (main) direction
+                     * because of choosing the right (counter)clockwise draw vector
+                     * use LINE_OVERLAP_MAJOR to fill all pixel
+                     *
+                     * EXAMPLE:
+                     * 1,2 = Pixel of first lines
+                     * 3 = Pixel of third line in normal line mode
+                     * - = Pixel which will additionally be drawn in LINE_OVERLAP_MAJOR mode
+                     *           33
+                     *       3333-22
+                     *   3333-222211
+                     * 33-22221111
+                     *  221111                     /\
+                     *  11                          Main direction of draw vector
+                     *  -> Line main direction
+                     *  <- Minor direction of counterclockwise draw vector
+                     */
+                    tOverlap = Overlap.LINE_OVERLAP_MAJOR;
+                }
+                tError += tDeltaYTimes2;
+                this.DrawLineOverlap(aXStart, aYStart, aXEnd, aYEnd, tOverlap, aColor);
+            }
+        }
+        else {
+            // the other octant
+            if (tSwap) {
+                tStepX = -tStepX;
+            }
+            else {
+                tDrawStartAdjustCount = (aThickness - 1) - tDrawStartAdjustCount;
+                tStepY = -tStepY;
+            }
+            // adjust draw start point
+            tError = tDeltaXTimes2 - tDeltaY;
+            for (i = tDrawStartAdjustCount; i > 0; i--) {
+                aYStart -= tStepY;
+                aYEnd -= tStepY;
+                if (tError >= 0) {
+                    aXStart -= tStepX;
+                    aXEnd -= tStepX;
+                    tError -= tDeltaYTimes2;
+                }
+                tError += tDeltaXTimes2;
+            }
+            //draw start line
+            this.DrawLineOverlap(aXStart, aYStart, aXEnd, aYEnd, Overlap.LINE_OVERLAP_NONE, aColor);
+            tError = tDeltaXTimes2 - tDeltaY;
+            for (i = aThickness; i > 1; i--) {
+                aYStart += tStepY;
+                aYEnd += tStepY;
+                tOverlap = Overlap.LINE_OVERLAP_NONE;
+                if (tError >= 0) {
+                    aXStart += tStepX;
+                    aXEnd += tStepX;
+                    tError -= tDeltaYTimes2;
+                    tOverlap = Overlap.LINE_OVERLAP_MAJOR;
+                }
+                tError += tDeltaXTimes2;
+                this.DrawLineOverlap(aXStart, aYStart, aXEnd, aYEnd, tOverlap, aColor);
+            }
+        }
+    }
+    public void Load() {
+        if(this.subs == null) {
+            this.subs = new Block[3];
+        }
+        for (int i = 0; i < this.subs.Length; i++) {
+            if (this.subs[i] != null) {
+                this.subs[i].root = this;
+                this.subs[i].Load();
+            }
+        }
+    }
+}
